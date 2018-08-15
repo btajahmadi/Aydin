1d284268aa4178894f00cd7262ede5336c5c94a9
A-SHAREGHI5865 <shareghiaydin@gmail.com>
2018-08-15 10:27:39 +0430

    gulpfile changed

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,9 @@ var gulp = require('gulp');
 
 gulp.task('compose-libraries', function() {
     gulp.src('node_modules/bootstrap/dist/**')
-    .pipe(gulp.dest('dist/lib/bootstrap-4.1.3'))
+    .pipe(gulp.dest('dist/lib/bootstrap-4.1.3'));
     gulp.src('node_modules/popper.js/dist/**')
     .pipe(gulp.dest('dist/lib/popper.js-1.14.4'));
     gulp.src('node_modules/jquery/dist/**')
     .pipe(gulp.dest('dist/lib/jquery-3.3.1'));
-
-
 })
